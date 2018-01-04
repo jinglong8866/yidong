0444bf521facde3906130427856d49b7865c82d3
jinglong8866 <470243670@qq.com>
2018-01-04 09:10:55 +0800

    q

--- js/index.js
+++ js/index.js
@@ -64,3 +64,85 @@
 		})
 	})
 } 
+
+
+
+{
+	{
+	let topbar = document.querySelector('.head2')
+	let floors = document.querySelectorAll("body .louceng")
+	let nav = document.querySelector(".youdao")
+	let navs = document.querySelectorAll(".youdao li")
+	let toptop = document.querySelector(".back")
+	let spans=document.querySelectorAll(".youdao span")
+	let flag = true;
+//	let navz=["享优惠","业务推荐","手机专区","智能硬件","咪咕娱乐"]
+//	let navN=["1F","1F","1F","1F","1F"]
+	console.log(navs)
+	window.onscroll = function() {
+		if(flag) {
+			let st = document.documentElement.scrollTop;
+			if(st > 550) {
+				topbar.style.display = 'block'
+			} else {
+				topbar.style.display = 'none'
+			}
+			if(st > 650) {
+				nav.style.display = "block"
+			} else {
+				nav.style.display = "none"
+			}
+			floors.forEach(function(ele, index) {
+				if(st < floors[0].offsetTop) {
+					for(let i = 0; i < navs.length; i++) {
+						navs[i].classList.remove("active")
+					}
+					navs[0].classList.add("active")
+				}
+				if(st >= ele.offsetTop-61) {
+					for(let i = 0; i <navs.length; i++) {
+						navs[i].classList.remove("active")
+						spans[i].classList.remove("active")
+//						navs[index].innerHTML=navN[index]
+					}
+					navs[index].classList.add("active")
+					spans[index].classList.add("active")
+//					navs[index].innerHTML=navz[index]
+				}
+			})
+		}
+	}
+	navs.forEach(function(ele, index) {
+		ele.onclick = function() {
+			flag = false;
+			let st = floors[index].offsetTop;
+			st -= 60
+			let now = document.documentElement.scrollTop;
+			let speed = (st - now) * 30 / 300;
+			let time = 0;
+			let t = setInterval(function() {
+				now += speed;
+				time += 30;
+				if(time === 300) {
+					clearInterval(t);
+					now = st;
+					flag = true;
+				}
+				document.documentElement.scrollTop = now;
+			}, 30)
+		}
+	})
+	toptop.onclick = function() {
+		let st = document.documentElement.scrollTop
+		let speed = st * 30 / 500
+		let t = setInterval(function() {
+			st -= speed
+			if(st <= 0) {
+				st = 0;
+				clearInterval(t)
+			}
+			document.documentElement.scrollTop = st
+		}, 30)
+	}
+}
+}
